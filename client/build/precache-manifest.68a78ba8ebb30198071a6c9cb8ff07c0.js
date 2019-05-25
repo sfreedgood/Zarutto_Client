e9100c43ca73c605f0fc6b8683383b715d1b224d
samfreedgood <sam.freedgood@gmail.com>
2019-05-25 16:43:55 -0400

    use existing local version to create branch, initializing server for project

---
+++ client/build/precache-manifest.68a78ba8ebb30198071a6c9cb8ff07c0.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a66a833e768bd3ea80e46defee64250",
+    "url": "/static/media/ramen1.5a66a833.png"
+  },
+  {
+    "revision": "46b44e5116337efcf916",
+    "url": "/static/css/main.9db9c41d.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "4c64b3268ad75f076adcb379be333e87",
+    "url": "/static/media/zurutto_abot.4c64b326.jpg"
+  },
+  {
+    "revision": "2f3f3ce8af7737bd9eab",
+    "url": "/static/js/2.2f3f3ce8.chunk.js"
+  },
+  {
+    "revision": "861bdffb9212f6a460da18b1e0c58517",
+    "url": "/static/media/appetizer-banner.861bdffb.jpg"
+  },
+  {
+    "revision": "3c31a155a69affa021496e91dfa79bce",
+    "url": "/static/media/dessert-banner.3c31a155.jpg"
+  },
+  {
+    "revision": "ee577bae290a6118201a81ecfde8b2cc",
+    "url": "/static/media/drink-banner.ee577bae.jpg"
+  },
+  {
+    "revision": "56236f09c947bc1836f3a27ced0b51a3",
+    "url": "/static/media/main-banner.56236f09.jpg"
+  },
+  {
+    "revision": "5bc0fb44a002c336719e5ddef35128b4",
+    "url": "/static/media/splash.5bc0fb44.png"
+  },
+  {
+    "revision": "46b44e5116337efcf916",
+    "url": "/static/js/main.46b44e51.chunk.js"
+  },
+  {
+    "revision": "cc1735e2360e27e2f211a1e5bbe546ea",
+    "url": "/static/media/ramen2.cc1735e2.png"
+  },
+  {
+    "revision": "1285ab45540c787957536a2f232d6c5a",
+    "url": "/static/media/ramen3.1285ab45.png"
+  },
+  {
+    "revision": "191e82072273633cd15f4630afe92978",
+    "url": "/static/media/ramen4.191e8207.png"
+  },
+  {
+    "revision": "103c99aa62054cb68dc3f391a6fcd5e6",
+    "url": "/static/media/logo.103c99aa.png"
+  },
+  {
+    "revision": "4c688dbfb8a9b1171424191270ba4e33",
+    "url": "/static/media/twit.4c688dbf.svg"
+  },
+  {
+    "revision": "cda2d393e10d10ac539f015a5f2499d7",
+    "url": "/static/media/instagram.cda2d393.svg"
+  },
+  {
+    "revision": "e3d3a3935b21f782c90bc9b7c6ca555d",
+    "url": "/static/media/facebook.e3d3a393.svg"
+  },
+  {
+    "revision": "b00843022635c3afa8aec113288254bc",
+    "url": "/static/media/background.b0084302.png"
+  },
+  {
+    "revision": "2f3f3ce8af7737bd9eab",
+    "url": "/static/css/2.c6b3301d.chunk.css"
+  },
+  {
+    "revision": "0a1d38d649ebc783fa029a27b97388bb",
+    "url": "/index.html"
+  }
+];
